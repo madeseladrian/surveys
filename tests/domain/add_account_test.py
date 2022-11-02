40bd12d33d516dddfbc69edccd5b59817c02357b
mades eladrian <mades.eladrian@gmail.com>
2022-11-02 15:47:20 -0300

    ensure AddAccount raises a NotImplementedError if not implemented

--- tests/domain/add_account_test.py
+++ tests/domain/add_account_test.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from inspect import isabstract
+import pytest
 from typing import TypedDict
+from unittest.mock import patch
 
 class AddAccountParams(TypedDict):
   name: str
@@ -18,3 +20,16 @@ class AddAccount(ABC):
 class TestAddAccount:
   def test_1_should_AddAccount_is_an_abstract_class(self):
     assert isabstract(AddAccount)
+
+  @patch.multiple(AddAccount, __abstractmethods__=set())
+  def test_2_should_AddAccount_raise_a_NotImplementedError_if_not_implemented(self):
+    params: AddAccountParams = AddAccountParams(
+      name='any_name',
+      email='any_email@any.com',
+      password='any_password'
+    )
+    addAccount: AddAccount = AddAccount()
+
+    with pytest.raises(NotImplementedError) as exc_info:
+      addAccount.add(account=params)
+    assert exc_info.value.args[0] == 'Should implement method: add'
