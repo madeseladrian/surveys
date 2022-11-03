dd021089c6a157859bad7a8255dee81fd4820d73
mades eladrian <mades.eladrian@gmail.com>
2022-11-02 22:53:25 -0300

    ensure CheckAccountByEmailRepository raises a NotImplementedError if not implemented

--- requirements/data/contracts/check_account_by_email_repository.md
+++ requirements/data/contracts/check_account_by_email_repository.md
@@ -3,4 +3,4 @@
 > ### Exceções
 
 1. ✅ **Verificar** se **CheckAccountByEmailRepository** é uma classe abstrata
-2. ❌ **Retorna** um error **NotImplementedError** se a class não for implementada
+2. ✅ **Retorna** um error **NotImplementedError** se a class não for implementada
--- tests/data/contracts/db/check_account_by_email_repository_test.py
+++ tests/data/contracts/db/check_account_by_email_repository_test.py
@@ -1,7 +1,16 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.data.contracts.db import CheckAccountByEmailRepository
 
 class TestCheckAccountByEmailRepository:
   def test_1_should_CheckAccountByEmailRepository_is_an_abstract_class(self):
     assert isabstract(CheckAccountByEmailRepository)
+
+  @patch.multiple(CheckAccountByEmailRepository, __abstractmethods__=set())
+  def test_2_should_CheckAccountByEmailRepository_raise_a_NotImplementedError_if_not_implemented(self):
+    checkAccountByEmailRepository = CheckAccountByEmailRepository()
+
+    with pytest.raises(NotImplementedError, match='Should implement method: check_by_email'):
+      checkAccountByEmailRepository.check_by_email(email='')
