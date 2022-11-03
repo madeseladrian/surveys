ea7265423516370e3b348c908d93163c3dad8f01
mades eladrian <mades.eladrian@gmail.com>
2022-11-02 22:35:43 -0300

    ensure Hasher raises a NotImplementedError if not implemented

--- requirements/data/contracts/hasher.md
+++ requirements/data/contracts/hasher.md
@@ -3,4 +3,4 @@
 > ### Exceções
 
 1. ✅ **Verificar** se **Hasher** é uma classe abstrata
-2. ❌ **Retorna** um error **NotImplementedError** se a class não for implementada
+2. ✅ **Retorna** um error **NotImplementedError** se a class não for implementada
--- tests/data/contracts/cryptography/hasher_test.py
+++ tests/data/contracts/cryptography/hasher_test.py
@@ -1,7 +1,16 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
 
 from src.data.contracts.cryptography import Hasher
 
 class TestController:
   def test_1_should_Hasher_is_an_abstract_class(self):
     assert isabstract(Hasher)
+
+  @patch.multiple(Hasher, __abstractmethods__=set())
+  def test_2_should_Hasher_raise_a_NotImplementedError_if_not_implemented(self):
+    hasher = Hasher()
+
+    with pytest.raises(NotImplementedError, match='Should implement method: hash'):
+      hasher.hash(plaintext='')
