b3647d00b767de739b2668e31352b3ccff0b271c
mades eladrian <mades.eladrian@gmail.com>
2022-11-11 22:52:12 -0300

    ensure EmailValidator raises a NotImplementedError if not implemented

--- tests/validation/contracts/email_validator_test.py
+++ tests/validation/contracts/email_validator_test.py
@@ -1,6 +1,16 @@
 from inspect import isabstract
+import pytest
+from unittest.mock import patch
+
 from src.validation.contracts import EmailValidator
 
 class TestAddAccount:
   def test_1_should_EmailValidator_is_an_abstract_class(self):
     assert isabstract(EmailValidator)
+
+  @patch.multiple(EmailValidator, __abstractmethods__=set())
+  def test_2_should_EmailValidator_raise_a_NotImplementedError_if_not_implemented(self):
+    email_validator = EmailValidator()
+
+    with pytest.raises(NotImplementedError, match='Should implement method: is_valid'):
+      email_validator.is_valid(email='any_email@any.com')
