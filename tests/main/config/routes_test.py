bb5019366af4ca13271effe4be6a12a6ed99de5c
mades eladrian <mades.eladrian@gmail.com>
2022-11-12 01:05:07 -0300

    ensure routes import api routes

---
+++ tests/main/config/routes_test.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from src.main.config import create_routes
+
+class TestApp:
+  app = FastAPI()
+  create_routes(app)
+
+  def test_1_should_calls_with_routes(self):
+    routes = self.app.router.routes[4:]
+    # path_routes = [route.__dict__['path'] for route in routes]
+
+    assert len(routes) > 0
