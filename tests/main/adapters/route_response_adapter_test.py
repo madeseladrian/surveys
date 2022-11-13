bf339fbc65970ca33707810eb8fc22be26439ab4
mades eladrian <mades.eladrian@gmail.com>
2022-11-12 23:42:49 -0300

    ensure route response adapter returns a bad request error

--- tests/main/adapters/route_response_adapter_test.py
+++ tests/main/adapters/route_response_adapter_test.py
@@ -1,3 +1,6 @@
+from fastapi import HTTPException
+import pytest
+
 from src.main.adapters import route_response_adapter
 from src.presentation.helpers import HttpResponse
 
@@ -9,3 +12,15 @@ class TestRouteResponseAdapter:
     }
 
     assert route_response_adapter(http_response) == http_response
+
+  def test_2_should_adapter_return_a_bad_request_error(self):
+    http_response: HttpResponse = {
+      'status_code': 400,
+      'body': 'Bad Request Error'
+    }
+    with pytest.raises(HTTPException) as excinfo:
+      route_response_adapter(http_response)
+
+    assert isinstance(excinfo.value, HTTPException)
+    assert excinfo.value.status_code == http_response['status_code']
+    assert excinfo.value.detail == http_response['body']
