8727305ed77a0b9103eeacf123d4a1d3ed9b0c74
mades eladrian <mades.eladrian@gmail.com>
2022-11-14 17:58:12 -0300

    ensure AuthenticationController calls Authentication with correct values

--- src/presentation/controllers/login_controller.py
+++ src/presentation/controllers/login_controller.py
@@ -1,11 +1,14 @@
 from dataclasses import dataclass
 
+from ...domain.features import Authentication
 from ..contracts import Validation
 from ..params import LoginControllerRequest
 
 @dataclass
 class LoginController():
+  authentication: Authentication
   validation: Validation
 
   def handle(self, request: LoginControllerRequest) -> None:
     self.validation.validate(request)
+    self.authentication.auth(request)
--- tests/presentation/controllers/authentication_controller_test.py
+++ tests/presentation/controllers/authentication_controller_test.py
@@ -2,31 +2,41 @@ from faker import Faker
 from typing import Tuple
 
 from src.domain.params import AuthenticationParams
+from src.domain.features import Authentication
 
 from src.presentation.contracts import Controller, Validation
 from src.presentation.controllers import LoginController
 
 from ...domain.mocks import mock_authentication_params
-from ..mocks import ValidationSpy
+from ..mocks import AuthenticationSpy, ValidationSpy
 
 class TestAuthenticationController:
   # SetUp
   faker = Faker()
   params: AuthenticationParams = mock_authentication_params()
 
-  SutTypes = Tuple[Controller, Validation]
+  SutTypes = Tuple[Controller, Authentication, Validation]
 
   def make_sut(self) -> SutTypes:
+    authentication_spy: Authentication = AuthenticationSpy()
     validation_spy: Validation = ValidationSpy()
     sut: Controller = LoginController(
+      authentication=authentication_spy,
       validation=validation_spy
     )
 
-    return sut, validation_spy
+    return sut, authentication_spy, validation_spy
 
   def test_1_should_call_Validation_with_correct_values(self):
-    sut, validation_spy = self.make_sut()
+    sut, _, validation_spy = self.make_sut()
     request = self.params
     sut.handle(request=request)
 
     assert validation_spy.value == request
+
+  def test_2_should_call_Authentication_with_correct_values(self):
+    sut, authentication_spy, _ = self.make_sut()
+    request = self.params
+    sut.handle(request=request)
+
+    assert authentication_spy.params == request
