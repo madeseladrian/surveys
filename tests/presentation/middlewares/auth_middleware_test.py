6185ab7a05bed16f89ef07d0760f3002c4b62a00
mades eladrian <mades.eladrian@gmail.com>
2022-11-21 21:10:32 -0300

    ensure AuthMiddleware returns 403 if LoadAccountByToken returns None

--- tests/presentation/middlewares/auth_middleware_test.py
+++ tests/presentation/middlewares/auth_middleware_test.py
@@ -24,16 +24,23 @@ class TestAuthMiddleware:
         )
         return sut, load_account_by_token_spy
 
-    def test_1_should_return_403_if_no_x_access_token_exists_in_headers(self):
-        sut, _ = self.make_sut()
-        http_response = sut.handle({})
-
-        assert http_response == forbidden(AccessDeniedError())
-
-    def test_2_should_call_LoadAccountByToken_with_correct_access_token(self):
+    def test_1_should_call_LoadAccountByToken_with_correct_access_token(self):
         role = 'any_role'
         sut, load_account_by_token_spy = self.make_sut(role)
         sut.handle(self.params)
 
         assert load_account_by_token_spy.access_token == self.params['access_token']
         assert load_account_by_token_spy.role == role
+
+    def test_2_should_return_403_if_no_x_access_token_exists_in_headers(self):
+        sut, _ = self.make_sut()
+        http_response = sut.handle({})
+
+        assert http_response == forbidden(AccessDeniedError())
+
+    def test_3_should_return_403_if_LoadAccountByToken_returns_None(self):
+        sut, load_account_by_token_spy = self.make_sut()
+        load_account_by_token_spy.result = None
+        http_response = sut.handle(self.params)
+
+        assert http_response == forbidden(AccessDeniedError())
